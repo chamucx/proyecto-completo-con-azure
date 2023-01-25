511d68eb2c7a72fe2df92573fc6d31389d416ceb
Antonio <joseantoniosysnet@gmail.com>
2023-01-25 05:28:02 -0500

    first commit

---
+++ ProyectoCompletoConAzure/Data/Migrations/20230124233949_MyInitialProjectMigration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoCompletoConAzure.Data.Migrations
+{
+    public partial class MyInitialProjectMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
